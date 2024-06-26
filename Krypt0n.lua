e9a3c75999868fc0ada9d633249bf6c40fc0c992
Krypton <109706035+RENBex6969@users.noreply.github.com>
2024-06-26 15:21:34 +0800

    Update Krypt0n.lua

--- Krypt0n.lua
+++ Krypt0n.lua
@@ -1,8 +1,8 @@
 game:GetService("StarterGui"):SetCore("SendNotification",{
-Title = "Script is loading do not execute again!",
+Title = "Krypton!",
 Text = "Script is loading do not execute again", 
 Button1 = "Yes",
 Button2 = "Okay",
 Duration = 30 
 })
-loadstring(game:HttpGet(''))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/RENBex6969/KryptonReload/main/Krypton.lua'))()
