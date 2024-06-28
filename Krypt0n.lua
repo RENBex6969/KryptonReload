4f7eaf5d040d11b84bf0f3e6674381b5f0c0c16d
Krypton <109706035+RENBex6969@users.noreply.github.com>
2024-06-28 13:33:17 +0800

    Delete Krypt0n.lua

--- Krypt0n.lua
+++
@@ -1,8 +0,0 @@
-game:GetService("StarterGui"):SetCore("SendNotification",{
-Title = "Krypton!",
-Text = "Script is loading do not execute again", 
-Button1 = "Yes",
-Button2 = "Okay",
-Duration = 30 
-})
-loadstring(game:HttpGet('https://raw.githubusercontent.com/RENBex6969/KryptonReload/main/Krypton.lua'))()
