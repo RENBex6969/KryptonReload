2cb3ade6445cb8680e070a31b65d1cf036724d47
Krypton <109706035+RENBex6969@users.noreply.github.com>
2024-07-23 19:59:13 +0800

    Update RLR.lua

--- RLR.lua
+++ RLR.lua
@@ -1,4 +1,26 @@
---[!] Remote Loadstring Rerouter | RLR
-BootableURL = ''
-loadstring(game:HttpGet(BootableURL))()
-print('Passed RLR using BootableURL :' .. BootableURL)
+BootableURL = ""
+
+function notify(args1)
+  local players = game:GetService("Players")
+  local notificationData = {
+    Title = "[KryProtect] : LOG",
+    Icon = "rbxthumb://type=AvatarHeadShot&id="..players.LocalPlayer.UserId.."&w=180&h=180",
+    Duration = 5
+  }
+  
+  if args1 == false then
+    notificationData.Text = "Krypton is already loaded. Rejoin or try again!"
+  elseif args1 == true then
+    notificationData.Text = "Krypton is loading!"
+  end
+  
+  game:GetService("StarterGui"):SetCore("SendNotification", notificationData)
+end
+
+if _G.IsKryptonLoaded == nil then
+  _G.IsKryptonLoaded = true
+  loadstring(game:HttpGet(BootableURL))()
+  notify(true)
+else
+  notify(false)
+end
